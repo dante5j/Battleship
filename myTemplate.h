c356ae4a00a329f8b5452f77fb4027a7721cc743
dante5j <32501485+dante5j@users.noreply.github.com>
2017-10-04 00:00:27 -0400

    header file

---
+++ myTemplate.h
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define PAUSE system("pause")
+#define CLS system("cls")
+#define FLUSH myFlush()
+
+void myFlush() {
+	while (getchar() != '\n');
+}// end myFlush
